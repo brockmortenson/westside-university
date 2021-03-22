fe565d58f1aced4fb9d50786b707a624a6185309
Brock Mortenson <mortensonbrock@gmail.com>
2021-03-22 15:41:58 -0600

    I tried not to use the detailed instructions or the solution code and instead referenced my code from today's code along. I only checked the solution code to make sure I did not have any errors. I used the detailed instructions twice after getting stuck. I had to use the solution code on the last step because I honestly had no idea how to do it.

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      studentInfo: {
 
+      }
+    };
   }
+
+  componentDidMount() {
+    // Why do we return this?
+    return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(results => {
+      this.setState({ studentInfo: results.data })
+    })
+    .catch(err => console.log(err));
+  };
 
   render() {
     return (
       <div className="box">
+        {/* I couldn't figure out how to get it back to the students list but I was able to get it to go back to the classlist. I will continue trying... */}
+        <Link to='/'><button className='back-btn'>Go Back</button></Link>
         <h1>Student</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>{this.state.studentInfo.grade}</h3>
+        <h3>{this.state.studentInfo.email}</h3>
       </div>
     )
   }
